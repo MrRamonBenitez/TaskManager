ad20fadffa899f5d7f9a37d1bd95c7329270a145
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-02-09 23:58:04 +0500

    Pom.xml added

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,21 +1,33 @@
 package ru.netology.javacore;
 
-import com.google.gson.Gson;
-
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
 
-public class TodoServer {
-    //...
+public class TodoServer_My implements AutoCloseable {
+    int port;
+    Todos todos;
 
-    public TodoServer(int port, Todos todos) {
-        //...
+    public TodoServer_My(int port, Todos todos) {
+        this.port = port;
+        this.todos = todos;
     }
 
     public void start() throws IOException {
         System.out.println("Starting server at " + port + "...");
-        //...
+//        while (true) {
+//            try (ServerSocket serverSocket = new ServerSocket(port);
+//                 Socket clientSocket = serverSocket.accept();
+//                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+//                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//            ) {
+//                out.println("Greetings, good friend! This is Task Manager! Looking forward to your commands!");
+//                String msg = in.readLine();
+//
+//            }
+//        }
+    }
+
+    @Override
+    public void close() throws Exception {
+
     }
 }
