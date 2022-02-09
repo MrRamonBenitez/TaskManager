2ba36f900853202e1ede33abf07b218b71bcfe66
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-02-10 00:00:42 +0500

    Class TodoSever modified

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,33 +1,51 @@
 package ru.netology.javacore;
 
-import java.io.*;
+import com.google.gson.*;
 
-public class TodoServer_My implements AutoCloseable {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.lang.System.*;
+
+public class TodoServer implements AutoCloseable {
     int port;
     Todos todos;
 
-    public TodoServer_My(int port, Todos todos) {
+    public TodoServer(int port, Todos todos) {
         this.port = port;
         this.todos = todos;
     }
 
     public void start() throws IOException {
-        System.out.println("Starting server at " + port + "...");
-//        while (true) {
-//            try (ServerSocket serverSocket = new ServerSocket(port);
-//                 Socket clientSocket = serverSocket.accept();
-//                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-//                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//            ) {
-//                out.println("Greetings, good friend! This is Task Manager! Looking forward to your commands!");
-//                String msg = in.readLine();
-//
-//            }
-//        }
+        out.println ( "Starting server at " + port + "..." );
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(port);
+                 Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));)
+                {
+
+                out.println("Greetings, good friend! This is Task Manager! Looking forward to your commands!");
+                String json = in.readLine ();
+                incomingJsonProcessing(json);
+                }
+        }
+    }
+
+    private void incomingJsonProcessing(String json) {
+        GsonBuilder builder = new GsonBuilder ();
+        Gson gson = builder.create ();
+        String[] record = gson.fromJson (json, String[].class );
+        String typeCommand = record[0];
+        String task = record[1];
+        if ( "ADD".equals ( typeCommand ) ) todos.addTask ( task );
+        if ( "REMOVE".equals ( typeCommand ) ) todos.removeTask ( task );
+        if ( "GET ALL".equals ( typeCommand ) ) todos.getAllTasks ();
     }
 
     @Override
     public void close() throws Exception {
-
+        out.println ("Server close");
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -2,20 +2,36 @@ package ru.netology.javacore;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.lang.System.out;
 
 public class Todos {
-    //...
+    private final List<String> taskList = new ArrayList<>();
 
     public void addTask(String task) {
-        //...
+        if ( taskList.isEmpty () || !taskList.contains ( task ) ) {
+            taskList.add ( task );
+        } else out.printf ( "Task %s already exists in the list", task );
+//        taskList.stream()
+//                .sorted ()
+//                .forEach (System.out::println);
     }
 
     public void removeTask(String task) {
-        //...
+        if (taskList.isEmpty ()) out.println ( "Task list is empty!");
+        if ( taskList.contains ( task ) ) taskList.remove ( task );
+        else out.printf ( "Task %s does not exist in the list!", task);
+//        taskList.stream()
+//                .sorted ()
+//                .forEach (System.out::println);
     }
 
-    public String getAllTasks() {
-        //...
+    public void getAllTasks() {
+        String msg;
+//        taskList.stream()
+//            .sorted ()
+//            .forEach (System.out::println);
     }
 
 }
