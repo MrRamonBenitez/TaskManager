d64a4729e6ecda42fe41703a0494b636936d35b5
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-02-13 17:14:34 +0500

    Class Client reload

--- src/main/java/ru/netology/javacore/Client.java
+++ src/main/java/ru/netology/javacore/Client.java
@@ -22,6 +22,6 @@ public class Client {
             out.println("{ \"type\": \"ADD\", \"task\": \"task #" + pickRandomChar() + "\" }");
             System.out.println(in.readLine());
 
-          }
+        }
     }
 }
