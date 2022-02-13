16576549ec42d0cfd43d7840abcd465c4ef6a3b4
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-02-13 17:15:38 +0500

    Class TodoServer modified

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -1,10 +1,10 @@
 package ru.netology.javacore;
 
 import com.google.gson.*;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.stream.Collectors;
 
 import static java.lang.System.*;
 
@@ -13,44 +13,58 @@ public class TodoServer implements AutoCloseable {
     private final Todos todos;
 
     private final GsonBuilder builder = new GsonBuilder();
-    private final StringBuilder sb = new StringBuilder();
 
     public TodoServer(int port, Todos todos) {
         this.port = port;
         this.todos = todos;
     }
 
-    public void start() throws IOException {
+    public void start() {
         out.println("Starting server at " + port + "...");
 
-        while (true) {
-            try (ServerSocket serverSocket = new ServerSocket(port);
-                 Socket clientSocket = serverSocket.accept();
-                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
 
-                incomingJsonProcessing(in.readLine(), out);
+            while (true) {
+                incomingJsonProcessing(in, out);
             }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
+
     }
 
-    public void incomingJsonProcessing(String json, PrintWriter out) {
-        Gson gson = builder.create();
-        ClientCommand clientCommand = gson.fromJson(json, ClientCommand.class);
+    public void incomingJsonProcessing(BufferedReader in, PrintWriter out) throws IOException {
+        String json = in.readLine();
 
-        String typeCommand = clientCommand.getType();
-        String task = clientCommand.getTask();
+        if (null == json) {
+            out.println("Command not found");
+        } else {
+            Gson gson = builder.create();
+            ClientCommand clientCommand = gson.fromJson(json, ClientCommand.class);
 
-        if ("ADD".equals(typeCommand)) {
-            todos.addTask(task);
-            out.println(printActualTask(out));
-        }
-        if ("REMOVE".equals(typeCommand)) {
-            todos.removeTask(task);
-            out.println(printActualTask(out));
-        }
-        if ("GET ALL".equals(typeCommand)) {
-            out.println(todos.getAllTasks());
+            String typeCommand = clientCommand.getType();
+            String task = clientCommand.getTask();
+
+            switch (typeCommand) {
+                case "ADD": {
+                    todos.addTask(task);
+                    out.println(printActualTask(out));
+                    break;
+                }
+                case "REMOVE": {
+                    todos.removeTask(task);
+                    out.println(printActualTask(out));
+                    break;
+                }
+                case "GET ALL": {
+                    out.println(todos.getAllTasks());
+                    break;
+                }
+            }
         }
     }
 
@@ -59,13 +73,15 @@ public class TodoServer implements AutoCloseable {
             out.println("Task list is empty!");
             return null;
         } else {
-            for (String s : todos.getTaskList()) {
-                sb.append(s).append(" ");
-            }
-            return sb.toString();
+            return todos.getTaskList().stream()
+                            .sorted()
+                            .collect(Collectors.joining(" "));
         }
     }
 
     @Override
-    public void close() { out.println("Server close"); }
+    public void close() {
+        out.println("Server close");
+    }
 }
+
